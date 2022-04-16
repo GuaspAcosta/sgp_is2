8109069c0eae030f6720446281fc29129948d792
GuaspAcosta <68080939+GuaspAcosta@users.noreply.github.com>
2022-04-16 01:16:52 -0400

    Cambios en permisos y roles

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -6,20 +6,15 @@
         </a>
   
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li><a href="{{ route('home') }}" class="nav-link px-2 text-white">Home</a></li>
+          <li><a href="{{ route('home') }}" class="nav-link px-2 text-white">Inicio</a></li>
           @auth
             @role('admin')
-            <li><a href="{{ route('users.index') }}" class="nav-link px-2 text-white">Users</a></li>
+            <li><a href="{{ route('users.index') }}" class="nav-link px-2 text-white">Usuario</a></li>
             <li><a href="{{ route('roles.index') }}" class="nav-link px-2 text-white">Roles</a></li>
             @endrole
             {{--<li><a href="{{ route('posts.index') }}" class="nav-link px-2 text-white">Posts</a></li>--}}
           @endauth
         </ul>
-  
-        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
-          <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
-        </form>
-  
         @auth
           {{auth()->user()->name}}&nbsp;
           <div class="text-end">
@@ -29,9 +24,8 @@
   
         @guest
           <div class="text-end">
-            <a href="{{ route('login') }}" class="btn btn-outline-light me-2">Login</a>
-            <a href="{{ route('register') }}" class="btn btn-warning">Sign-up</a>
-          </div>
+            <a href="{{ route('login') }}" class="btn btn-outline-light me-2">Iniciar Sesión</a>
+            </div>
         @endguest
       </div>
 
--- resources/views/permissions/index.blade.php
+++ resources/views/permissions/index.blade.php
@@ -2,13 +2,13 @@
 
 @section('content')
     
-    <h1 class="mb-3">Laravel 8 User Roles and Permissions Step by Step Tutorial - codeanddeploy.com</h1>
+    <h1 class="mb-3">Listado Permisos</h1>
 
     <div class="bg-light p-4 rounded">
-        <h2>Permissions</h2>
+        <h2>Permisos</h2>
         <div class="lead">
-            Manage your permissions here.
-            <a href="{{ route('permissions.create') }}" class="btn btn-primary btn-sm float-right">Add permissions</a>
+            Administra tus permisos aqui.
+            <a href="{{ route('permissions.create') }}" class="btn btn-primary btn-sm float-right">Agregar permisos</a>
         </div>
         
         <div class="mt-2">
@@ -18,7 +18,7 @@
         <table class="table table-striped">
             <thead>
             <tr>
-                <th scope="col" width="15%">Name</th>
+                <th scope="col" width="15%">Nombre</th>
                 <th scope="col">Guard</th> 
                 <th scope="col" colspan="3" width="1%"></th> 
             </tr>
@@ -28,10 +28,10 @@
                     <tr>
                         <td>{{ $permission->name }}</td>
                         <td>{{ $permission->guard_name }}</td>
-                        <td><a href="{{ route('permissions.edit', $permission->id) }}" class="btn btn-info btn-sm">Edit</a></td>
+                        <td><a href="{{ route('permissions.edit', $permission->id) }}" class="btn btn-info btn-sm">Editar</a></td>
                         <td>
                             {!! Form::open(['method' => 'DELETE','route' => ['permissions.destroy', $permission->id],'style'=>'display:inline']) !!}
-                            {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-sm']) !!}
+                            {!! Form::submit('Eliminar', ['class' => 'btn btn-danger btn-sm']) !!}
                             {!! Form::close() !!}
                         </td>
                     </tr>
